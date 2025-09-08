771f984800f0305afcb8e06389b67b7175c3b44e
Joaogabriel6 <jgabriel2905@gmail.com>
2025-09-08 20:38:36 -0300

    atividade repetição final

---
+++ 016-atividadeRepeticaoFinal.js
@@ -0,0 +1,11 @@
+/* 
+    crie um programa que peça ao usuario um valor inicial e um valor
+    final para gerar tabuadas do valor inicial até o final.
+*/
+
+let entrada = require('prompt-sync')();
+
+let vinicial = (entrada("Digite um valor inicial: "));
+let vfinal = (entrada("Digite um valor final: "));
+let result = 0;
+contador = 0;
